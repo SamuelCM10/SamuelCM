1c216aa3c947315537bdf9d76d6f1858d20c442b
ciclofp <ciclofp@10.0.2.15>
2023-09-28 14:18:18 +0200

    Primer commit

---
+++ ProyectoDAW/src/unidad1/ejemplos/Ejemplo1.java
@@ -0,0 +1,11 @@
+package unidad1.ejemplos;
+
+public class Ejemplo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+
+	}
+
+}
