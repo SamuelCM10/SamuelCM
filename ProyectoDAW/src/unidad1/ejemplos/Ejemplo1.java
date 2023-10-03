2a843a97e2e28409a2021143c1ff8af918f47ff8
ciclofp <ciclofp@10.0.2.15>
2023-10-03 12:06:43 +0200

    V1

--- ProyectoDAW/src/unidad1/ejemplos/Ejemplo1.java
+++ ProyectoDAW/src/unidad1/ejemplos/Ejemplo1.java
@@ -5,8 +5,6 @@ public class Ejemplo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
-		System.out.println("Hola Mundo");
-		System.out.println("Hola Mundo");
 
 
 	}
