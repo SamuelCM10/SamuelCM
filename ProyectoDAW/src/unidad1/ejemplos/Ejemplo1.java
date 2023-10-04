0be53f5e16d7fcc48d22bf077d7b7d84d34bf8a2
ciclofp <ciclofp@10.0.2.15>
2023-10-04 09:40:08 +0200

    Variables

--- ProyectoDAW/src/unidad1/ejemplos/Ejemplo1.java
+++ ProyectoDAW/src/unidad1/ejemplos/Ejemplo1.java
@@ -3,11 +3,26 @@ package unidad1.ejemplos;
 public class Ejemplo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	
-		String nombre = "Samuel";
-		System.out.print("Hola Mundo, me llamo ");
-		System.out.println(nombre);
+		
+	//Cliente
+		String dni = "28979276N";
+		String nombre = "Samuel Cordero Martín";
+		String direccion = "C/Mercado Nº20 1ºF";
+		String telefono = "722785803";
+		String cp = "10300";
+	//Animal
+		int id;
+		id = 333444555;
+		String nombrePet = "Mussolinni";
+		char sexo;
+		byte edad;
+		String raza;
+		float peso;
+		boolean estaVacunado;
+		boolean estaCastrado;
+		String motivoConsulta;
+		
+		
 	}
 
 }
