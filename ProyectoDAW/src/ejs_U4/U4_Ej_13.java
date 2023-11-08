e175b8fa05596418963d8565e00b60feba7583e7
ciclofp <ciclofp@10.0.2.15>
2023-11-08 10:19:26 +0100

    8 11 23

---
+++ ProyectoDAW/src/ejs_U4/U4_Ej_13.java
@@ -0,0 +1,92 @@
+package ejs_U4;
+
+import java.util.Scanner;
+
+public class U4_Ej_13 {
+
+		static double inversion;
+		static double tipoInteres;
+		static int numAños;
+		public static Scanner entrada=new Scanner(System.in);
+		
+	public static void main(String[] args) {
+		
+		/*
+		inversion=pedirInversion();
+		tipoInteres=pedirInteres();
+		numAños=pedirNumAños();
+		
+		Con return para que devuelva los datos, con double o int
+		Para modificar, se modifica cada función*/
+		
+		pedirDatos();
+		//recoger 3 datos a la vez, con void
+		//se toca menos código
+		
+		/*
+		pedirInversion2();
+		pedirInteres2();
+		pedirNumAños2();
+		
+		soo actualizan el dato, no hacen NADA MAS
+		
+		pedirDatos2();
+		
+		actualiza funciones con el 2*/
+	}
+	private static void pedirDatos() {
+		
+		System.out.println();
+		inversion=entrada.nextDouble();
+		System.out.println();
+		tipoInteres=entrada.nextDouble();
+		System.out.println();
+		numAños=entrada.nextInt();
+		
+	}
+	private static int pedirNumAños() {
+		
+		int numAños=0;
+		System.out.println();
+		numAños=entrada.nextInt();
+		return numAños;
+		
+	}
+	private static double pedirInteres() {
+		
+		double tipoInteres=0;
+		System.out.println();
+		tipoInteres=entrada.nextInt();
+		return tipoInteres;
+		
+	}
+	private static double pedirInversion() {
+	
+		double inversion=0;
+		System.out.println();
+		inversion=entrada.nextInt();
+		return inversion;
+	
+	}
+	private static void pedirInversion2() {
+		System.out.println("");
+		inversion=entrada.nextInt();
+		
+	}
+	private static void pedirInteres2() {
+		System.out.println("");
+		tipoInteres=entrada.nextInt();
+		
+	}
+	private static void pedirNumAños2() {
+		System.out.println("");
+		numAños=entrada.nextInt();
+		
+	}
+	private static void pedirDatos2() {
+		
+		pedirInversion2();
+		pedirInteres2();
+		pedirNumAños2();
+	}
+}
---
+++ ProyectoDAW/src/ejs_U4/U4_Ej_13_1.java
@@ -0,0 +1,35 @@
+package ejs_U4;
+
+import java.util.Scanner;
+
+public class U4_Ej_13_1 {
+
+		static double inversion=1000;
+		static double tipoInteres=10;
+		static int numAños=1;
+		public static Scanner entrada=new Scanner(System.in);
+		
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+	private static void imprimirVF() {
+		int i;
+		for (i=0;i<5; i++) {
+			System.out.print(inversion);
+			System.out.print(", ");
+			System.out.print(tipoInteres);
+			System.out.print(", ");
+			System.out.print(numAños);
+			System.out.print(".");
+			numAños++;
+			i++;
+		}
+	}	
+	private static void calcularVF() {
+		double valorFinal;
+		
+	}
+}
