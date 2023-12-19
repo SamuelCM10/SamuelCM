41719839c00af8e8c033ecc17a9b4b331c077154
ciclofp <ciclofp@10.0.2.15>
2023-12-19 11:14:44 +0100

    Orientado a objetos completar

---
+++ ProyectoDAW/src/unidad7/ejemplos/AplicaciónBicicletas.java
@@ -0,0 +1,17 @@
+package unidad7.ejemplos;
+
+public class AplicaciónBicicletas {
+
+	public static void main(String[] args) {
+		
+		Bicicleta bici = new Bicicleta(1,2,"Orbea","X34","ROJO");
+		Bicicleta bici2 = new Bicicleta(2,4,"BH","XTREME","NEGRO");
+		
+		bici.imprimirDatos();
+		
+		bici.setMarca("Orbea");
+		bici.imprimirDatos();
+
+	}
+
+}
---
+++ ProyectoDAW/src/unidad7/ejemplos/Bicicleta.java
@@ -0,0 +1,30 @@
+package unidad7.ejemplos;
+
+public class Bicicleta {
+
+	public static void main(String[] args) {
+		
+		private String marca="Sin marca";
+		private String modelo="Sin modelo";
+		private String color="No definido";
+		private int id=0;
+		private int tipo=0;
+		
+		public Bicicleta(int id,int tipo,String color,String modelo,String marca) {
+			this.marca=marca;
+			this.modelo=modelo;
+			this.color=color;
+			this.id=id;
+			this.tipo=tipo;
+		}
+		
+		public String getMarca() {
+			return marca;
+		}
+		public String getMarca() {
+			return marca;
+		}
+
+	}
+
+}
