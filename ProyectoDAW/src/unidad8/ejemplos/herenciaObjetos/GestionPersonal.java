596969c11fc68d713cf156770ebe29a8afe4a5a6
diurno <diurno@equipo-00>
2024-02-15 14:24:30 +0100

    12 2 24

---
+++ ProyectoDAW/src/unidad8/ejemplos/herenciaObjetos/GestionPersonal.java
@@ -0,0 +1,23 @@
+package unidad8.ejemplos.herenciaObjetos;
+
+public class GestionPersonal {
+
+	public static void main(String[] args) {
+		
+		Persona p1 = new Persona("Manuel");
+		Persona p2 = new Persona("Sancho");
+		p1.setLocalidad("Málaga");
+		p2.setLocalidad("Berlín");
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		
+		if(p1.getNombre().equals(p2.getNombre())) {
+			System.out.println("Son la misma persona.");
+		}else{
+			System.out.println("No son la misma persona.");
+		}
+
+	}
+
+}
---
+++ ProyectoDAW/src/unidad8/ejemplos/herenciaObjetos/Persona.java
@@ -0,0 +1,60 @@
+package unidad8.ejemplos.herenciaObjetos;
+
+public class Persona {
+
+	private int id;
+	private String nombre;
+	private String localidad;
+	private static int contador = 1000;
+	
+	public Persona(String nombre) {
+		this.id = contador;
+		contador++;
+		this.nombre = nombre;
+	}
+	@Override
+	public String toString() {
+		
+		return this.id+"-"+this.nombre+"-"+this.localidad;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		
+		boolean sonIguales=false;
+		if(this==obj) {
+			sonIguales=true;
+		}
+		if(obj instanceof Persona) {
+			Persona laOtraPersona = (Persona) obj;
+			sonIguales = this.nombre.equals(laOtraPersona.getNombre())
+					&& this.localidad.equals(laOtraPersona.getLocalidad());
+		}
+		return sonIguales;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public static int getContador() {
+		return contador;
+	}
+	public static void setContador(int contador) {
+		Persona.contador = contador;
+	}
+	
+	
+}
